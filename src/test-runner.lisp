48bede6ee2622c31c1cf4c21c7bff2e5d4602f8a
Francis St-Amour <m_psyco2@hotmail.com>
2019-07-21 16:19:10 -0400

    add control-loop to worker
    This simplifies the test-runner a lot

--- src/test-runner.lisp
+++ src/test-runner.lisp
@@ -15,23 +15,14 @@
 
 (in-package #:breeze.test-runner)
 
-;; TODO Get rid of this global
-(defvar *stop-test-runner* nil
-  "If true, the test-runner-thread will stop on the next iteration.")
+(defclass test-runner (worker)
+  ((messages :initform ())
+   (received-messages-last-iteration-p :initform nil)))
 
-(defclass test-runner (worker) ())
+(defvar *test-runner* (make-instance 'test-runner
+                                     :interval 0.25
+                                     :name "test runner"))
 
-(defvar *test-runner* (make-instance 'test-runner :interval 0.25))
-
-(defmethod worker-report ((test-runner test-runner) level control-string &rest format-arguments)
-  (declare (ignore test-runner level))
-  (when nil ;; don't log anything anywhere for now
-    (fresh-line)
-    (apply #'format t control-string format-arguments)
-    (fresh-line)
-    (force-output)))
-
-;; (defmethod worker-process-messages ((test-runner test-runner) message-list)
 (defun process-messages (message-list)
   "Take a list of messages (already debounced) and deduplicate and process them."
   (let ((tests (make-hash-table))
@@ -44,48 +35,29 @@
                (if (gethash test-name tests)
                    (push message skipped)
                    (setf (gethash test-name tests) t))))
-    (run-all-tests (hash-table-keys tests))
-    #+nil
-    (format t "~&Would run: ~A~%Would skip: ~A~%"
-            (hash-table-keys tests)
-            skipped)
-    #+nil
-    (force-output)))
+    (run-all-tests (hash-table-keys tests))))
 
 (defun receive-messages ()
   (worker-receive-all-messages *test-runner*))
 
-(defun test-runner-loop (test-runner)
-  (let ((messages ())
-        (received-messages-last-iteration-p nil))
-    (worker-report test-runner :info "test runner started")
-    (loop :until *stop-test-runner*
-          :do
-             (worker-report test-runner :debug "~%running test runner~%Messages: ~A~%received: ~A"
-                            messages received-messages-last-iteration-p)
-             (aif (receive-messages)
-                  (setf messages (append messages it)
-                        received-messages-last-iteration-p t)
-                  (setf received-messages-last-iteration-p nil))
-             (when (and messages (not received-messages-last-iteration-p))
-               (process-messages messages)
-               (setf received-messages-last-iteration-p nil
-                     messages nil))
-             (sleep (worker-interval test-runner)))
-    (worker-report test-runner :info "test runner stopped")))
+(defmethod worker-report ((test-runner test-runner) level control-string &rest format-arguments)
+  ;; Make the test-worker quiet (do nothing)
+  ;; (call-next-method) ;; <- print to standard-output
+  )
 
-(defmethod worker-loop ((test-runner test-runner))
-  (test-runner-loop test-runner))
-
-;; around methods are awesome
-(defmethod worker-start :around ((test-runner test-runner))
-  "Start the test runner thread"
-  (setf *stop-test-runner* nil)
-  (call-next-method))
-
-(defmethod worker-stop ((test-runner test-runner))
-  "Stop the test runner thread."
-  (setf *stop-test-runner* t))
+(defmethod worker-run ((test-runner test-runner))
+  (with-slots (messages received-messages-last-iteration-p)
+      test-runner
+    (worker-report test-runner :debug "~%running test runner~%Messages: ~A~%received: ~A"
+                   messages received-messages-last-iteration-p)
+    (aif (receive-messages)
+         (setf messages (append messages it)
+               received-messages-last-iteration-p t)
+         (setf received-messages-last-iteration-p nil))
+    (when (and messages (not received-messages-last-iteration-p))
+      (process-messages messages)
+      (setf received-messages-last-iteration-p nil
+            messages nil))))
 
 (defun ensure-test-runner ()
   (worker-ensure-alive *test-runner*))
@@ -100,8 +72,10 @@
 
 ;; Bunch of calls used to manually test the test-runner's logic
 #|
-(start-test-runner)
-(stop-test-runner)
+(worker-start *test-runner*)
+(worker-stop *test-runner*)
+(worker-alive-p *test-runner*)
+
 (request-to-run-test 'a)
 (request-to-run-test* '(a b c)))
 (progn
--- src/worker.lisp
+++ src/worker.lisp
@@ -13,7 +13,7 @@
    #:worker-receive-all-messages
    #:worker-report
    #:worker-process-messages
-   #:worker-loop
+   #:worker-run
    #:worker-start
    #:worker-stop
    #:worker-alive-p
@@ -23,7 +23,11 @@
 (in-package #:breeze.worker)
 
 (defclass worker ()
-  ((input-channel
+  ((name
+    :accessor worker-name
+    :initarg :name
+    :initform "worker")
+   (input-channel
     :accessor worker-input-channel
     :initarg :input-channel
     :initform (make-instance 'chanl:unbounded-channel))
@@ -31,6 +35,10 @@
     :accessor worker-output-channel
     :initarg :output-channel
     :initform nil)
+   (control-channel
+     :accessor worker-control-channel
+     :initarg :control-channel
+     :initform (make-instance 'chanl:unbounded-channel))
    (interval
     :accessor worker-interval
     :initarg :interval
@@ -38,9 +46,20 @@
     :type 'positive-real
     :documentation "The time between each iteration worker's loop.
 Accepts positive reals (see cl:sleep function).")
+   (control-interval
+    :accessor worker-control-interval
+    :initarg :control-interval
+    :initform 0.1
+    :type 'positive-real
+    :documentation "The time between each worker's contol loop
+Accepts positive reals (see cl:sleep function).")
+   (last-iteration-time
+    :accessor worker-last-iteration-time
+    :initform nil
+    :type 'positive-real
+    :documentation "Time that the the worker was called last.")
    (task
     :accessor worker-task
-    ;; :initarg :task
     :initform nil))
   (:documentation "Workers are threads that process messages send via a channel."))
 
@@ -53,24 +72,59 @@ Accepts positive reals (see cl:sleep function).")
           :collect el)))
 
 (defgeneric worker-report (worker level control-string &rest format-arguments)
-  (:documentation "Log a string."))
+  (:documentation "Log a string.")
+  (:method ((worker worker) level control-string &rest format-arguments)
+    (fresh-line)
+    (apply #'format t control-string format-arguments)
+    (fresh-line)
+    (force-output)))
 
 (defgeneric worker-process-messages (worker message-list)
   (:documentation "Process a batch of messages."))
 
-(defgeneric worker-loop (worker)
-  (:documentation "The function that is run in a thread by worker-start"))
+(defun worker-set-last-iteration-time (worker)
+  (setf (worker-last-iteration-time worker) (/ (get-internal-real-time)
+                                               internal-time-units-per-second)))
+
+(defgeneric worker-run (worker)
+  (:documentation "The function that is called periodically.")
+  (:method ((worker worker))
+    (worker-report worker :debug "running ~a"
+                   (worker-name worker))))
+
+(defgeneric worker-control-loop (worker)
+  (:documentation "The function that is run in a thread by worker-start")
+  (:method ((worker worker))
+    (worker-report worker :info "~a started"
+                   (worker-name worker))
+    (loop :for message = (chanl:recv (worker-control-channel worker) :blockp nil)
+          :until (eq message :stop)
+          :do
+             (worker-report worker :debug "running ~a's control loop"
+                            (worker-name worker))
+             (let ((current-time (/ (get-internal-real-time)
+                                    internal-time-units-per-second))
+                   (last-time (worker-last-iteration-time worker)))
+               (when (or (null last-time)
+                         (> (- current-time last-time)
+                            (worker-interval worker)))
+                 (worker-set-last-iteration-time worker)
+                 (worker-run worker)))
+             (sleep (worker-control-interval worker)))
+    (worker-report worker :info "~a stopped"
+                   (worker-name worker))))
 
 (defgeneric worker-start (worker)
   (:documentation "Start the worker's task")
   (:method ((worker worker))
     (setf (worker-task worker) (chanl:pcall
-                                #'(lambda () (worker-loop worker))
-                                ;; TODO :name "test-runner"
-                                ))))
+                                #'(lambda () (worker-control-loop worker))
+                                :name (worker-name worker)))))
 
 (defgeneric worker-stop (worker)
-  (:documentation "Stop the worker's task"))
+  (:documentation "Stop the worker's task")
+  (:method ((worker worker))
+    (chanl:send (worker-control-channel worker) :stop)))
 
 (defgeneric worker-alive-p (worker)
   (:documentation "Check if the worker's task is running.")
@@ -89,4 +143,14 @@ Accepts positive reals (see cl:sleep function).")
   (:method ((worker worker) message)
     (chanl:send (worker-input-channel worker) message)))
 
+
+
+#|
+(defparameter *test-worker*
+  (make-instance 'worker
+                 :name "test worker"))
+
+(worker-start *test-worker*)
+(worker-stop *test-worker*)
+|#
 
