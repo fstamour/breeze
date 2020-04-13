1a1293b814b4ca0a7d2237de7fa4da9610a896fb
Francis St-Amour <m_psyco2@hotmail.com>
2020-04-13 11:01:17 -0400

    fix doc.nix to be able to run under eshell

--- doc.nix
+++ doc.nix
@@ -1,12 +1,11 @@
 #!/usr/bin/env nix-shell
-#!nix-shell ./shell.nix --command "rlwrap sbcl --non-interactive --eval '(ql:quickload (list :staple :staple-markdown :breeze))' --eval '(staple:generate :breeze :if-exists :supersede)'"
+#!nix-shell ./shell.nix --command "sbcl --non-interactive --eval '(ql:quickload (list :staple :staple-markdown :breeze))' --eval '(staple:generate :breeze :if-exists :supersede)' --eval '(terpri)'"
 
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
-	  rlwrap
     sbcl
   ];
 }
