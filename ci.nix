749a897451d21fa0683c940a858cb74a35a23446
Francis St-Amour <m_psyco2@hotmail.com>
2020-08-22 16:22:45 -0400

    fix ci.nix
    I think

--- ci.nix
+++ ci.nix
@@ -1,6 +1,9 @@
 #!/usr/bin/env nix-shell
-#!nix-shell ./shell.nix --command "./ci.sh";
+#!nix-shell ./ci.nix --command "./ci.sh"
 
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
     lisp-quicklisp
