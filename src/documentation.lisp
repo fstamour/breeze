cff1d6f0bf842167b67c701bf9c99a8a0a87a6c3
Francis St-Amour <m_psyco2@hotmail.com>
2020-09-12 15:19:35 -0400

    fix indentation (just 1 line)

--- src/documentation.lisp
+++ src/documentation.lisp
@@ -234,8 +234,7 @@
 	     (gen "Classes" (classp symbol) 'type)
 	     (gen "Generic methods" (generic-method-p symbol) 'function)
 	     (gen "Functions" (simple-function-p symbol) 'function)
-	     (gen "Macros" (macrop symbol) 'function))
-	   ))))
+	     (gen "Macros" (macrop symbol) 'function))))))
 
 (defun generate-documentation-to-stream (stream)
   (let ((spinneret:*html* stream))
@@ -267,4 +266,3 @@
 	 :if-does-not-exist :create)
       (generate-documentation-to-stream output)
       (format t "~%breeze.documentation: ~s written.~%" index))))
-
