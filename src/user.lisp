6d03d0e65b53ccc2c851d4f9aae3d03748cfc92b
Francis St-Amour <fr.stamour@gmail.com>
2021-09-02 19:16:08 -0400

    fix typos

--- src/user.lisp
+++ src/user.lisp
@@ -81,11 +81,11 @@
   (format t "~&~{ * ~A~%~}"
 	  (remove-if #'null
 		     `(#+later "Remember to use the emacs mode if applicable."
-		       "User (br:next) to get hints on what to do next."
+		       "Use (br:next) to get hints on what to do next."
 		       "Use \"br\" as a nickname for \"breeze.user\" (e.g. `br:main` instead of `breeze.user:main`)."
 		       "Use (require 'swank) followed by (swank:create-server) to start swank."
 		       "Once swank is started, call (breeze.swank:advise-swank-interactive-eval)"
-		       "User (br:dogfood) to start hacking on breeze."
+		       "Use (br:dogfood) to start hacking on breeze."
 		       ,(maybe-tips-about-test-runner nil))))
   "Breeze started!")
 
