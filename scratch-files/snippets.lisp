15353f7451355ae2ee32dd16d6b5c490d5214c8e
Francis St-Amour <m_psyco2@hotmail.com>
2021-09-10 21:44:12 -0400

    clean a bit the "snippets"

--- scratch-files/snippets.lisp
+++ scratch-files/snippets.lisp
@@ -1,17 +1,26 @@
-;;;; WARN: THIS WAS PROTOTYPED USING MYELIN
-
-
 ;;; Snippets
 ;;; N.B. They are more like templates/skeletons.
+;;;
+;;; - The macro "define-snippet" generate a function and register the snippet
+;;; - Snippets always takes a stream-designator as a first arguement
+;;;
+;;; Major things that could be improved:
+;;;   - The snippet's lambda-list
+;;;   - The snippet's name (currently "snippet/the-name")
+;;;   - Ability to ask the user for input
+
 
 (in-package #:common-lisp-user)
 
-(defpackage #:breeze.snippets
-  (:use :cl))
+(defpackage breeze.snippets
+  (:use :cl)
+  (:import-from #:alexandria
+		#:ensure-car
+		#:symbolicate))
 
 (in-package #:breeze.snippets)
 
-;; (ql:quickload 'with-output-to-stream)
+(ql:quickload '(str with-output-to-stream))
 
 (defvar *snippet-stream* nil
   "The stream use to print out snippets.")
@@ -63,9 +72,10 @@
 			    docstring
 			  &body body)
   "Create a function and register a snippet."
-  (check-type docstring string)
+  (check-type docstring string)		; Docstrings are required
   `(progn
-     (defun ,(alexandria:symbolicate 'snippet/ name)
+     ;; First definition, where the lambda-list is just "optional"
+     (defun ,(symbolicate 'snippet/ name)
 	 (stream
 	  &optional
 	    ,@(mapcar #'ensure-car lambda-list))
@@ -74,6 +84,7 @@
        (let ((*indentation* *indentation*))
 	 (with-output-to-stream:with-output-to-stream (*snippet-stream* stream)
 	   ,@(transform-snippet body))))
+     ;; Second definition, where the lambda-list are keywords
      (defun ,(symbolicate 'snippet/ name '*)
 	 (stream
 	  &key
@@ -108,12 +119,12 @@
 
 (define-snippet defun ((name :placeholder "Name of the function")
 		       (lambda-list :placeholder "Argument list"))
-    "Insert a defun form."
+		"Insert a defun form."
   (indent)
   "(defun " 'name
   " (" (when lambda-list
 	 (format *snippet-stream* "~(~{~a~^ ~}~)"
-		 (ensure-list lambda-list)))
+		 (alexandria:ensure-list lambda-list)))
   ")" > \n
   _ ")")
 
@@ -138,172 +149,13 @@
   _ ")")
 
 #+nil
-(snip/petdefmacro "my-macro")
+(snippet/defmacro nil "my-macro")
 
 (defun snippet-function-symbol (snippet)
+  "Given a snippet name, return the symbol of the function implementing the snippet."
   (let ((*package* #.*package*))
     (symbolicate 'snippet/ snippet)))
 
 (defun snippet-function-symbol* (snippet)
   (let ((*package* #.*package*))
     (symbolicate 'snippet/ snippet '*)))
-
-
-
-(defvar *code-id-counter* 0)
-(defparameter *code* (make-hash-table))
-
-;; (clrhash *code*)
-
-(define-easy-handler (code :uri "/code")
-    ((id :parameter-type 'integer)
-     (snippet :parameter-type 'string))
-  (let ((id (or id (incf *code-id-counter*))))
-    (with-output-to-string (*html*)
-      (with-html ("Code")
-	(:ul
-	 (loop
-	    :for snippet :being :the :hash-key :of *snippets*
-	    :for fn = (snippet-function-symbol snippet)
-	    :do (:li
-		 (:form :method "post"
-			:action "/code/add-snippet-form"
-			(:input :type "hidden"
-				:value id
-				:name "id")
-			(:input :type "hidden"
-				:name "snippet"
-				:value snippet)
-			(:input :type "submit"
-				:value (format nil "Insert ~(~a~)" snippet))))))
-	(unless (gethash id *code*)
-	  (setf (gethash id *code*) (list (format nil ";;; Code ~a" id))))
-	(loop
-	   :for block :in (gethash id *code*)
-	   :for block-id :from 0
-	   :do
-	     (:article (:pre block))
-	     (:form :method "post"
-			:action "/code/edit-block"
-			(:input :type "hidden"
-				:value id
-				:name "id")
-			(:input :type "hidden"
-				:name "block-id"
-				:value block-id)
-			(:input :type "submit"
-				:value "Edit"))
-	     (:form :method "post"
-			:action "/code/delete-block"
-			(:input :type "hidden"
-				:value id
-				:name "id")
-			(:input :type "hidden"
-				:name "block-id"
-				:value block-id)
-			(:input :type "submit"
-				:value "Delete")))
-	))))
-
-(define-easy-handler (add-snippet-form :uri "/code/add-snippet-form")
-    ((id :parameter-type 'integer)
-     (snippet :parameter-type 'string))
-  (with-output-to-string (*html*)
-    (with-html ("Add snippet to code")
-      (:h2 (format nil "Snippet \"~(~a~)\"" snippet))
-      (:form :method "post"
-	     :action "/code/add-snippet"
-	     (:input :type "hidden"
-		     :value id
-		     :name "id")
-	     (:input :type "hidden"
-		     :name "snippet"
-		     :value snippet)
-	     (loop
-		:for spec :in (snippet-inputs
-			       (intern snippet #.*package*))
-		:do (render-input spec) (:br))
-	     (:input :type "submit")))))
-
-(define-easy-handler (add-snippet :uri "/code/add-snippet")
-    ((id :parameter-type 'integer)
-     (snippet :parameter-type 'string))
-  (setf (gethash id *code*)
-	;; Append the current code with the result of calling the snippet.
-	(append
-	 (gethash id *code*)
-	 (list
-	  ;; Call the snippet function
-	  (apply (snippet-function-symbol* snippet)
-		 nil
-		 ;; Transform the parameters to keywords
-		 (loop :for (key value) :on
-		      (hash-table-plist
-		       (cl-hash-util:collecting-hash-table ()
-			 (loop
-			    :for (key . value) :in (post-parameters*)
-			    :do
-			      (unless (member key '("id" "snippet")
-					      :test 'string=)
-				(when (and value
-					   (> (length value) 0))
-				  (cl-hash-util:collect
-				      (make-keyword (string-upcase key))
-				    value))))))
-		    :by #'cddr
-		    :append (list key
-				  (if (cdr value)
-				      value
-				      (car value))))))))
-  (redirect (format nil "/code?id=~a" id)))
-
-(cl-hash-util:collecting-hash-table
-    (:mode (list
-	    #'(lambda (current new)
-		(append (ensure-list current)
-			(list new)))
-	    #'(lambda (new)
-		new)))
-  (cl-hash-util:collect :a 1)
-  (cl-hash-util:collect :a 2)
-  (cl-hash-util:collect :b 1)
-  )
-
-(loop
-   :for snippet :being :the :hash-key :of *snippets*
-   :for fn = (snippet-function-symbol snippet)
-   :collect (list fn (documentation fn 'function)))
-
-(define-easy-handler (snippet :uri "/code/snippet")
-    ((snippet :parameter-type 'string))
-  (with-output-to-string (*html*)
-    (with-html ("Snippet")
-      (:p
-       (format nil snippet)
-       ;; Warning: internalizing strings from the internet _might_ not be a good idea
-       (let ((snippet (intern (string-upcase snippet))))
-	 (if (gethash snippet *snippets*)
-	     (:ul
-	      (multiple-value-bind (required optional)
-		  (parse-ordinary-lambda-list
-		   (third
-		    (gethash snippet *snippets*)))
-		(loop :for arg :in required
-		   :do (:li (format nil "~a" arg)))
-		(loop :for (arg default _) :in optional
-		   :do (:li (format nil "~a" arg)))))
-	     "Not found"))))))
-
-
-
-(define-easy-handler (listing :uri "/code/listing")
-    ()
-  (with-output-to-string (*html*)
-    (with-html ("Listing")
-      (:h2 "Defpackage")
-      (:article
-       (:pre
-	  (snippet/defpackage *html* "dummy")))
-      (:article
-       (:pre
-	"(defun x2 (x) (* 2 x))")))))
