2b5965e86422a8ceb6973e506b191918935a9366
Francis St-Amour <fr.stamour@gmail.com>
2021-11-16 21:06:09 -0500

    add some pieces of code that would need attention

---
+++ examples/lint.lisp
@@ -0,0 +1,26 @@
+
+(if (null x)
+    ()
+  t)
+=> x
+
+(if x
+    x
+  nil)
+=>
+(when x)
+
+(when (and x y) y)
+=>
+(and x y)
+
+
+a_symbol_with_underscores
+
+(+(- 3 2) 4)
+
+
+(if
+    pain
+    tylenol
+)
