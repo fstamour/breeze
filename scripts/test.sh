a342c27bf5597e1ee81b5cd85ba93a7888abfab6
Francis St-Amour <fr.stamour@gmail.com>
2023-03-01 22:39:05 -0500

    fix: test.sh exits with 0 when there are failing tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,9 @@
 
 cd "$(git rev-parse --show-toplevel)"
 
-sbcl --non-interactive \
+exec sbcl --non-interactive \
      --eval "(declaim (optimize (debug 3) (speed 0) (safety 3)))" \
      --eval "(ql:quickload '#:breeze/test)" \
-     --eval "(asdf:test-system '#:breeze)"
+     --eval "(breeze.test.main:run-breeze-tests t)"
+
 # TODO make sure the exit-value is ok
