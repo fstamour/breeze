4c6cdcb064984e9b50deb29d693faabc9a8dbaff
Francis St-Amour <fr.stamour@gmail.com>
2023-03-01 22:39:05 -0500

    fix: test.sh exits with 0 when there are failing tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -2,15 +2,18 @@
 
 (defpackage #:breeze.test.main
   (:documentation "Entry point for all tests in breeze.")
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:run-breeze-tests))
 
 (in-package #:breeze.test.main)
 
-(defun run-breeze-tests ()
+(defun run-breeze-tests (&optional exitp)
   "Run breeze's tests."
   (let ((packages (breeze.xref:find-packages-by-prefix "breeze.test")))
     (format *debug-io*
             "~&About to run tests for the packages: ~{~A~^, ~}"
             packages)
     (finish-output *debug-io*)
-    (parachute:test packages)))
+    (if exitp
+        (parachute:test-toplevel packages)
+        (parachute:test packages))))
