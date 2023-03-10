7493aaa6de9296508225fba826c384748fccd40c
Francis St-Amour <fr.stamour@gmail.com>
2023-03-09 19:56:06 -0500

    add thread.lisp (moved utilities out of breeze-listener)

---
+++ src/thread.lisp
@@ -0,0 +1,54 @@
+(uiop:define-package #:breeze.thread
+    (:documentation "Utilities to help with concurrent programming.")
+  (:use #:cl)
+  (:import-from #:breeze.xref
+                #:function-designator-p)
+  (:export
+   #:find-threads
+   #:find-threads-by-name
+   #:find-worker-threads
+   #:kill-threads
+   #:kill-threads-by-name
+   #:kill-worker-threads))
+
+(in-package #:breeze.thread)
+
+
+;;; Thread management
+
+(defun find-threads (&optional predicate (exclude-self-p t))
+  (let ((current-thread (when exclude-self-p (bt:current-thread))))
+    (remove-if-not #'(lambda (thread)
+                       (and (not (eq current-thread thread))
+                            (if predicate
+                                (funcall predicate thread)
+                                t)))
+                   (bt:all-threads))))
+
+(defun find-threads-by-name (name &key (exclude-self-p t))
+  (find-threads #'(lambda (thread)
+                    (string= (bt:thread-name thread) name))
+                exclude-self-p))
+
+(defun find-worker-threads (&optional (exclude-self-p t))
+  (find-threads-by-name "worker" :exclude-self-p nil))
+
+(defun %kill-threads (threads)
+  (prog1
+      ;; Always return the number of threads
+      (length threads)
+    (when threads
+      (mapcar #'bordeaux-threads:destroy-thread threads)
+      (format *debug-io* "~&Killed ~d threads.~%" (length threads)))))
+
+(defun kill-threads (predicate)
+  "Find threads by predicate, and destroy them."
+  (%kill-threads (find-threads predicate)))
+
+(defun kill-threads-by-name (name)
+  "Find threads by name, then destroy them."
+  (%kill-threads (find-threads-by-name name)))
+
+(defun kill-worker-threads ()
+  "Find threads named \"worker\", then destroy them."
+  (%kill-threads (find-threads-by-name "worker")))
