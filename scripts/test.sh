1b1baabe0c4ceb2417d77be8c6e765d012c5a39d
Francis St-Amour <fr.stamour@gmail.com>
2023-04-17 23:10:26 -0400

    #29 ci: fix job passes even if the test fails

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,8 @@
 
 cd "$(git rev-parse --show-toplevel)"
 
-exec sbcl --non-interactive \
+exec sbcl --noinform --non-interactive \
      --eval "(declaim (optimize (debug 3) (speed 0) (safety 3)))" \
+     --eval "(asdf:load-asd (truename \"breeze.asd\"))" \
      --eval "(ql:quickload '#:breeze/test)" \
      --eval "(breeze.test.main:run-breeze-tests t)"
-
-# TODO make sure the exit-value is ok
