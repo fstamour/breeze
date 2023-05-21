e6b100a7016646c03d694a7f0c948ef016e75fbc
Francis St-Amour <fr.stamour@gmail.com>
2023-05-20 21:38:09 -0400

    refactor reader2's tests to use parachute

--- tests/reader2.lisp
+++ tests/reader2.lisp
@@ -1,4 +1,56 @@
 
+(cl:in-package #:cl-user)
+
+(defpackage #:breeze.test.reader2
+  (:documentation "Test package for #:breeze.reader2")
+  (:use #:cl #:breeze.reader2)
+  (:import-from #:breeze.reader2
+                #:+end+
+                #:at
+                #:block-comment
+                #:donep
+                #:find-all
+                #:line-comment
+                #:make-state
+                #:node
+                #:not-terminatingp
+                #:parens
+                #:parse
+                #:parse*
+                #:pos
+                #:punctuation
+                #:read-block-comment
+                #:read-char*
+                #:read-line-comment
+                #:read-parens
+                #:read-punctuation
+                #:read-quoted-string
+                #:read-string
+                #:read-string*
+                #:read-token
+                #:read-whitespaces
+                #:search-or
+                #:source
+                #:token
+                #:valid-position-p
+                #:whitespace
+                ;; Symbols used in the returns
+                #:quote ; this ones from cl actually
+                #:quasiquote
+                #:dot
+                #:at
+                #:comma
+                #:sharp)
+  (:import-from #:parachute
+                #:define-test
+                #:define-test+run
+                #:is
+                #:true
+                #:false
+                #:of-type))
+
+(in-package #:breeze.test.reader2)
+
 #|
 
 Testing strategies
@@ -15,15 +67,6 @@ sense (e.g. a line comment should start with a ; and end with a
 newline or +end+)
 
 |#
-
-#++
-(defpackage #:breeze.test.reader2
-  (:documentation "")
-  (:use #:cl #:breeze.reader2))
-#++
-(in-package #:breeze.test.reader2)
-
-(in-package #:breeze.reader2)
 
 
 ;;; Reader state
@@ -46,12 +89,12 @@ newline or +end+)
                  (*print-circle* t)
                  (*print-right-margin* nil))
              (format *debug-io* "~&~a" str)
-             str)))
-    (if expectedp
-        (if (equalp got expected)
-            t
-            (fmt "~A «~a» got: ~s expected: ~s" *ctx* input got expected))
-        (fmt "~A «~a» => ~s" *ctx* input got))))
+             str))
+         (*print-pretty* nil))
+    #++ (true expectedp "~A «~a» => ~s" *ctx* input got)
+    #++ (is equalp expected got "~A «~a» got: ~s expected: ~s" *ctx* input got expected)
+    (true expectedp "«~a» => ~s" input got)
+    (is equalp expected got "For «~a»~%~tgot:~%~t~t~s~%~texpected:~%~t~t~s" input got expected)))
 
 #++
 (let ((*ctx* "Testing the \"test\" function."))
@@ -94,8 +137,7 @@ newline or +end+)
 
 ;;; Reader position (in the source string)
 
-;; TODO valid-position-p
-(let ((*ctx* 'valid-position-p))
+(define-test+run valid-position-p
   (with-state*
     (""
      (test* (valid-position-p state -1) nil)
@@ -106,95 +148,106 @@ newline or +end+)
      (test* (valid-position-p state 0) t)
      (test* (valid-position-p state 1) nil))))
 
-;; TODO donep
-(with-state*
-  ("" (test* (donep state) t))
-  (" " (test* (donep state) nil))
-  ("  " (test* (donep state) nil)))
+(define-test+run donep
+  :depends-on (valid-position-p)
+  (with-state*
+    ("" (test* (donep state) t))
+    (" " (test* (donep state) nil))
+    ("  " (test* (donep state) nil))))
 
 
 ;;; Getting and comparing characters
 
-;; TODO at
-(with-state*
-  (""
-   (test* (at state -1) nil)
-   (test* (at state 0) nil)
-   (test* (at state 1) nil)
-   (test* (at state -1 #\a) nil)
-   (test* (at state 0 #\b) nil)
-   (test* (at state 1 #\c) nil))
-  ("c"
-   (test* (at state -1) nil)
-   (test* (at state 0) #\c)
-   (test* (at state 1) nil)
-   (test* (at state -1 #\c) nil)
-   (test* (at state 0 #\c) #\c)
-   (test* (at state 0 #\a) nil)
-   (test* (at state 1 #\c) nil)))
+(define-test+run at
+  :depends-on (valid-position-p)
+  (with-state*
+    (""
+     (test* (at state -1) nil)
+     (test* (at state 0) nil)
+     (test* (at state 1) nil)
+     (test* (at state -1 #\a) nil)
+     (test* (at state 0 #\b) nil)
+     (test* (at state 1 #\c) nil))
+    ("c"
+     (test* (at state -1) nil)
+     (test* (at state 0) #\c)
+     (test* (at state 1) nil)
+     (test* (at state -1 #\c) nil)
+     (test* (at state 0 #\c) #\c)
+     (test* (at state 0 #\a) nil)
+     (test* (at state 1 #\c) nil))))
+
+;; TODO test "current-char"
+(define-test+run current-char)
+
+;; TODO test "next-char"
+(define-test+run next-char)
+
 
 
 ;;; Low-level parsing helpers
 
-;; TODO read-char*
-(with-state*
-  (""
-   (test* (list (read-char* state) (pos state)) '(nil 0))
-   (test* (list (read-char* state #\a) (pos state)) '(nil 0)))
-  ("c"
-   (test* (list (read-char* state) (pos state)) '(#\c 1))
-   (test* (list (read-char* state #\d) (pos state)) '(nil 0))))
+(define-test+run read-char*
+  :depends-on (current-char)
+  (with-state*
+    (""
+     (test* (list (read-char* state) (pos state)) '(nil 0))
+     (test* (list (read-char* state #\a) (pos state)) '(nil 0)))
+    ("c"
+     (test* (list (read-char* state) (pos state)) '(#\c 1))
+     (test* (list (read-char* state #\d) (pos state)) '(nil 0)))))
 
-;; TODO read-string*
-(with-state*
-  (""
-   (test*
-    (list
-     (read-string* state "")
-     (pos state))
-    '(nil 0))
-   (test*
-    (list
-     (read-string* state "#")
-     (pos state))
-    '(nil 0)))
-  (";"
-   (test*
-    (list
-     (read-string* state ";;")
-     (pos state))
-    '(nil 0)))
-  (";;"
-   (test*
-    (list
-     (read-string* state ";;")
-     (pos state))
-    '((0 2) 2))))
+(define-test+run read-string*
+  :depends-on (valid-position-p)
+  (with-state*
+    (""
+     (test*
+      (list
+       (read-string* state "")
+       (pos state))
+      '(nil 0))
+     (test*
+      (list
+       (read-string* state "#")
+       (pos state))
+      '(nil 0)))
+    (";"
+     (test*
+      (list
+       (read-string* state ";;")
+       (pos state))
+      '(nil 0)))
+    (";;"
+     (test*
+      (list
+       (read-string* state ";;")
+       (pos state))
+      '((0 2) 2)))))
 
 ;; TODO test read-while
+(define-test+run read-while)
 
-
-;; TODO find-all
 (defun test-find-all (needle string expected)
   (test (list 'find-all needle string)
         (find-all needle string)
         expected))
-(list
- (test-find-all "" "" nil)
- (test-find-all "a" "" nil)
- (test-find-all "" "a" nil)
- (test-find-all "a" "aaa" '(0 1 2))
- (test-find-all "b" "aaa" nil))
 
-;; TODO search-for
-(defun test-search-for (needles input expected)
+(define-test+run find-all
+  (test-find-all "" "" nil)
+  (test-find-all "a" "" nil)
+  (test-find-all "" "a" nil)
+  (test-find-all "a" "aaa" '(0 1 2))
+  (test-find-all "b" "aaa" nil))
+
+(defun test-search-or (needles input expected)
   (with-state (input)
     (test input (search-or needles state) expected)))
-(list
- (test-search-for '("a" "b") "" nil)
- (test-search-for '("a" "b") "c" nil)
- (test-search-for '("a" "b") "-ab" 1)
- (test-search-for '("a" "b") "--ba" 2))
+
+(define-test+run search-or
+  (test-search-or '("a" "b") "" nil)
+  (test-search-or '("a" "b") "c" nil)
+  (test-search-or '("a" "b") "-ab" 1)
+  (test-search-or '("a" "b") "--ba" 2))
 
 
 ;;; Actual reader
@@ -206,11 +259,13 @@ newline or +end+)
       (test* (read-whitespaces state)
              (when expected-end
                (whitespace 0 expected-end))))))
-(list
- (test-read-whitespaces "" nil)
- (test-read-whitespaces "a" nil)
- (test-read-whitespaces " " 1)
- (test-read-whitespaces "  " 2))
+
+(define-test+run read-whitespaces
+  ;; :depends-on (whitespacep)
+  (test-read-whitespaces "" nil)
+  (test-read-whitespaces "a" nil)
+  (test-read-whitespaces " " 1)
+  (test-read-whitespaces "  " 2))
 
 ;; TODO read-block-comment
 (defun test-read-block-comment (input expected-end &rest children)
@@ -220,35 +275,37 @@ newline or +end+)
             (when expected-end
               (block-comment 0 expected-end children))))))
 
-(list
- (test-read-block-comment "" nil)
- (test-read-block-comment "#|" +end+)
- (test-read-block-comment "#| " +end+)
- (test-read-block-comment "#||#" 4)
- (test-read-block-comment "#|#" +end+)
- (test-read-block-comment "#|#|#" +end+ (block-comment 2 +end+))
- (test-read-block-comment "#|#||##" +end+ (block-comment 2 6))
- (test-read-block-comment "#|#|#|#" +end+
-                          (block-comment 2 +end+
-                                         (block-comment 4 +end+)))
- (test-read-block-comment "#|#|#||##" +end+
-                          (block-comment 2 +end+
-                                         (block-comment 4 8)))
- (test-read-block-comment "#|#||#|##"
-                          ;; There's 9 characters, the last # is not part of any comments
-                          8 (block-comment 2 6)))
+;; TODO Fixme :/
+(define-test+run read-block-comment
+  :depends-on (read-string* search-or)
+  (test-read-block-comment "" nil)
+  (test-read-block-comment "#|" +end+)
+  (test-read-block-comment "#| " +end+)
+  (test-read-block-comment "#||#" 4)
+  (test-read-block-comment "#|#" +end+)
+  (test-read-block-comment "#|#|#" +end+ (block-comment 2 +end+))
+  (test-read-block-comment "#|#||##" +end+ (block-comment 2 6))
+  (test-read-block-comment "#|#|#|#" +end+
+                           (block-comment 2 +end+
+                                          (block-comment 4 +end+)))
+  (test-read-block-comment "#|#|#||##" +end+
+                           (block-comment 2 +end+
+                                          (block-comment 4 8)))
+  (test-read-block-comment "#|#||#|##"
+                           ;; There's 9 characters, the last # is not part of any comments
+                           8 (block-comment 2 6)))
 
-;; TODO read-line-comment
 (defun test-read-line-comment (input expected-end)
   (let ((*ctx* 'read-line-comment))
     (with-state ((format nil input))
       (test* (read-line-comment state)
              (when expected-end
                (line-comment 0 expected-end))))))
-(list
- (test-read-line-comment "" nil)
- (test-read-line-comment ";" +end+)
- (test-read-line-comment "; asdf~%" 7))
+
+(define-test+run read-line-comment
+  (test-read-line-comment "" nil)
+  (test-read-line-comment ";" +end+)
+  (test-read-line-comment "; asdf~%" 7))
 
 ;; TODO read-punctuation
 (defun test-read-punctuation (input expected-type)
@@ -258,71 +315,82 @@ newline or +end+)
             (read-punctuation state)
             (when expected-type
               (punctuation expected-type 0))))))
-(list
- (test-read-punctuation "" nil)
- (test-read-punctuation " " nil)
- (test-read-punctuation "'" 'quote)
- (test-read-punctuation "`" 'quasiquote)
- (test-read-punctuation "." 'dot)
- (test-read-punctuation "@" 'at)
- (test-read-punctuation "," 'comma)
- (test-read-punctuation "#" 'sharp)
- ;; anything else should return nil
- )
+
+(define-test+run read-punctuation
+  :depends-on (current-char)
+  (test-read-punctuation "" nil)
+  (test-read-punctuation " " nil)
+  (test-read-punctuation "'" 'quote)
+  (test-read-punctuation "`" 'quasiquote)
+  (test-read-punctuation "." 'dot)
+  (test-read-punctuation "@" 'at)
+  (test-read-punctuation "," 'comma)
+  (test-read-punctuation "#" 'sharp)
+  ;; anything else should return nil
+  )
 
 
-;; TODO read-quoted-string
 ;; TODO Add tests with VALIDP
-(let ((*ctx* 'read-quoted-string))
-  (list
-   (with-state ("")
-     (test* (read-quoted-string state #\| #\/) nil))
-   (with-state ("|")
-     (test* (read-quoted-string state #\| #\/) (list 0  +end+)))
-   (with-state ("||")
-     (test* (read-quoted-string state #\| #\/) '(0 2)))
-   (with-state ("| |")
-     (test* (read-quoted-string state #\| #\/) '(0 3)))
-   (with-state ("|/||")
-     (test* (read-quoted-string state #\| #\/) '(0 4)))
-   (with-state ("|/|")
-     (test* (read-quoted-string state #\| #\/) (list 0 +end+)))))
+(define-test+run read-quoted-string
+  :depends-on (at)
+  (with-state ("")
+    (test* (read-quoted-string state #\| #\/) nil))
+  (with-state ("|")
+    (test* (read-quoted-string state #\| #\/) (list 0  +end+)))
+  (with-state ("||")
+    (test* (read-quoted-string state #\| #\/) '(0 2)))
+  (with-state ("| |")
+    (test* (read-quoted-string state #\| #\/) '(0 3)))
+  (with-state ("|/||")
+    (test* (read-quoted-string state #\| #\/) '(0 4)))
+  (with-state ("|/|")
+    (test* (read-quoted-string state #\| #\/) (list 0 +end+))))
 
-;; TODO read-string
 (defun test-read-string (input expected-end)
   (let ((*ctx* 'read-string))
     (with-state (input)
       (test* (read-string state)
              (when expected-end
                (node 'string 0 expected-end))))))
-(list
- (test-read-string "" nil)
- (test-read-string "\"" +end+)
- (test-read-string "\"\"" 2)
- (test-read-string "\" \"" 3))
 
-;; TODO not-terminatingp
+(define-test+run read-string
+  :depends-on (read-quoted-string)
+  (test-read-string "" nil)
+  (test-read-string "\"" +end+)
+  (test-read-string "\"\"" 2)
+  (test-read-string "\" \"" 3))
 
-;; TODO read-token
+(define-test+run not-terminatingp
+  (mapcar #'(lambda (char)
+              (false (not-terminatingp char)
+                     "~c is supposed to be a terminating character." char))
+          '(#\; #\" #\' #\( #\) #\, #\`)))
+
 (defun test-read-token (input expected-end)
   (let ((*ctx* 'read-token))
     (with-state (input)
       (test* (read-token state)
              (when expected-end
                (token 0 expected-end))))))
-(list
- (test-read-token "" nil)
- (test-read-token " " nil)
- (test-read-token "+-*/" 4)
- (test-read-token "123" 3)
- (test-read-token "| asdf |" 8)
- (test-read-token "| asdf |qwer#" 13)
- (test-read-token "arg| asdf | " 11)
- (test-read-token "arg| asdf |more" 15)
- (test-read-token "arg| asdf |more|" +end+)
- (test-read-token "arg| asdf |more|mmoooore|done" 29)
- (test-read-token "arg| asdf |no  |mmoooore|done" 13))
 
+;; TODO Fix read-token
+(define-test+run read-token
+  :depends-on (current-char
+               not-terminatingp
+               read-quoted-string
+               read-while)
+  (test-read-token "" nil)
+  (test-read-token " " nil)
+  (test-read-token "+-*/" 4)
+  (test-read-token "123" 3)
+  (test-read-token "| asdf |" 8)
+  (test-read-token "| asdf |qwer#" 13)
+  (test-read-token "arg| asdf | " 11)
+  (test-read-token "arg| asdf |more" 15)
+  (test-read-token "arg| asdf |more|" +end+)
+  (test-read-token "arg| asdf |more|mmoooore|done" 29)
+  (test-read-token "arg| asdf |no  |mmoooore|done" 13)
+  (test-read-token "look|another\| case\| didn't think of|" 22))
 
 ;; TODO read-extraneous-closing-parens
 
@@ -332,18 +400,22 @@ newline or +end+)
     (test* (read-parens state)
            (when expected-end
              (parens 0 expected-end children)))))
-(list
- (test-read-parens ")" nil)
- (test-read-parens "(" +end+)
- (test-read-parens "()" 2)
- (test-read-parens "(x)" 3 (token 1 2))
- (test-read-parens "(.)" 3 (punctuation 'dot 1))
- (test-read-parens "( () )" 6
-                   (whitespace 1 2)
-                   (parens 2 4)
-                   (whitespace 4 5)))
+
+(define-test+run read-parens
+  :depends-on (read-char*)
+  (test-read-parens ")" nil)
+  (test-read-parens "(" +end+)
+  (test-read-parens "()" 2)
+  (test-read-parens "(x)" 3 (token 1 2))
+  (test-read-parens "(.)" 3 (punctuation 'dot 1))
+  (test-read-parens "( () )" 6
+                    (whitespace 1 2)
+                    (parens 2 4)
+                    (whitespace 4 5)))
 
 ;; TODO read-any
+(define-test read-any)
+
 
 
 ;;; Putting it all toghether
@@ -355,32 +427,32 @@ newline or +end+)
       (test input (parse* input) expected)
       (test input (parse* input))))
 
-(let ((*ctx* 'test-parse))
-  (list
-   (eq (parse "") nil)
-   (test-parse "  " (whitespace 0 2))
-   (test-parse "#|" (block-comment 0 +end+))
-   (test-parse " #| "
-               (whitespace 0 1)
-               (block-comment 1 +end+)
-               (whitespace 3 4))
-   (test-parse "#||#" (block-comment 0 4))
-   (test-parse "#|#||#" (block-comment 0 +end+ (block-comment 2 6)))
-   (test-parse "#| #||# |#" (block-comment 0 10 (block-comment 3 7)))
-   (test-parse "'" (punctuation 'quote 0))
-   (test-parse "`" (punctuation 'quasiquote 0))
-   (test-parse "#" (punctuation 'sharp 0))
-   (test-parse "," (punctuation 'comma 0))
-   (test-parse "+-*/" (token 0 4))
-   (test-parse "123" (token 0 3))
-   (test-parse "asdf#" (token 0 5))
-   (test-parse "| asdf |" (token 0 8))
-   (test-parse "arg| asdf | " (token 0 11) (whitespace 11 12))
-   (test-parse "arg| asdf |more" (token 0 15))
-   (test-parse "arg| asdf |more|" (token 0 +end+))
-   (test-parse ";" (line-comment 0 +end+))
-   (test-parse "(12" (parens 0 +end+ (token 1 3)))
-   (test-parse "\"" (node 'string 0 +end+))))
+(define-test+run "parse"
+  :depends-on (read-parens)
+  (eq (parse "") nil)
+  (test-parse "  " (whitespace 0 2))
+  (test-parse "#|" (block-comment 0 +end+))
+  (test-parse " #| "
+              (whitespace 0 1)
+              (block-comment 1 +end+)
+              (whitespace 3 4))
+  (test-parse "#||#" (block-comment 0 4))
+  (test-parse "#|#||#" (block-comment 0 +end+ (block-comment 2 6)))
+  (test-parse "#| #||# |#" (block-comment 0 10 (block-comment 3 7)))
+  (test-parse "'" (punctuation 'quote 0))
+  (test-parse "`" (punctuation 'quasiquote 0))
+  (test-parse "#" (punctuation 'sharp 0))
+  (test-parse "," (punctuation 'comma 0))
+  (test-parse "+-*/" (token 0 4))
+  (test-parse "123" (token 0 3))
+  (test-parse "asdf#" (token 0 5))
+  (test-parse "| asdf |" (token 0 8))
+  (test-parse "arg| asdf | " (token 0 11) (whitespace 11 12))
+  (test-parse "arg| asdf |more" (token 0 15))
+  (test-parse "arg| asdf |more|" (token 0 +end+))
+  (test-parse ";" (line-comment 0 +end+))
+  (test-parse "(12" (parens 0 +end+ (token 1 3)))
+  (test-parse "\"" (node 'string 0 +end+)))
 
 
 
