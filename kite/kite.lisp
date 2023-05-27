54378c32eb0399674d5fbee6fd00cd351c1328f3
Francis St-Amour <fr.stamour@gmail.com>
2023-05-26 22:06:43 -0400

    tests: add utility "is-equalp"

--- kite/kite.lisp
+++ kite/kite.lisp
@@ -1,6 +1,14 @@
 (uiop:define-package #:breeze.kite
     (:documentation "")
-  (:use #:cl))
+  (:use #:cl)
+  (:import-from #:parachute
+                #:define-test
+                #:define-test+run
+                #:is
+                #:true
+                #:false
+                #:of-type)
+  (:export #:is-equalp))
 
 (in-package #:breeze.kite)
 
@@ -22,3 +30,58 @@
   :for name = (parachute:name test)
   :when (stringp name)
     :do (parachute:remove-test test))
+
+
+(defun is-equalp (input got &optional (expected nil expectedp))
+  "Helper for testing that GOT and EXPECTED are EQUALP.
+
+It is more useful than a plain parachute:is because you can run it
+interactively, without an expected value. In which case it will return
+the value that was gotten.
+
+It will also print in the *debug-io*.
+
+The main point though, is that the error message is going to be nice.
+"
+  (let ((*print-pretty* nil)
+        (*print-circle* t)
+        (*print-right-margin* nil)
+        (control "For «~a»~%~tgot:~%~t~t~s~%~texpected:~%~t~t~s"))
+    (flet ((fmt (&rest args)
+             (let ((str (apply #'format nil args)))
+               (unless parachute:*context*
+                 (format *debug-io* "~&~a" str))
+               str)))
+      (if expectedp
+          (is equalp expected got control input got expected)
+          (true expectedp "«~a» => ~s" input got))
+      (unless (equalp expected got)
+        (fmt control input got expected))
+      got)))
+
+#|
+Examples
+
+(is-equalp "32 " 2)
+returns 2
+prints:
+For «32 »
+ got:
+  2
+ expected:
+  NIL
+
+(is-equalp "32 " 2 1)
+returns 2
+prints
+For «32 »
+ got:
+  2
+ expected:
+  1
+
+(is-equalp "32 " 2 2)
+returns 2
+doesn't print
+
+|#
