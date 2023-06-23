f1a6f5a81b6bbd64a158609d0fee9f320c2fcc28
Francis St-Amour <fr.stamour@gmail.com>
2023-06-23 08:40:39 -0400

    add quicklisp.lisp

---
+++ src/quicklisp.lisp
@@ -0,0 +1,30 @@
+(defpackage #:breeze.quicklisp
+  (:documentation "Utilities for quicklisp")
+  (:use #:cl))
+
+(in-package #:breeze.quicklisp)
+
+#++
+(ql-dist:enabled-dists)
+;; => (#<QL-DIST:DIST quicklisp 2023-02-15>)
+
+(defparameter *ql* (ql-dist:find-dist "quicklisp"))
+
+;; Download everything, without installing it.
+(loop :for release :in (ql-dist:provided-releases *ql*)
+      :do (ql-dist:ensure-local-archive-file release))
+
+#++
+(/ (length (ql-dist:installed-releases *ql*))
+   (length (ql-dist:provided-releases *ql*)))
+
+;; Get the total size (in bytes) of all the archives
+;; (require 'osicat)
+(loop :for release :in (ql-dist:installed-releases *ql*)
+      :sum (osicat-posix:stat-size
+            (osicat-posix:stat
+             (ql-dist:local-archive-file release))))
+;; => 615869163
+
+;; (float (/ 615869163 1024 1024))
+;; => 587.33
