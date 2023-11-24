8000bccd3bf93f573d1ebbc437e59a35d84a8bf5
Francis St-Amour <fr.stamour@gmail.com>
2023-11-23 23:39:40 -0500

    notes: add example of structural editing

--- scratch-files/notes/strutural-editing.lisp
+++ scratch-files/notes/strutural-editing.lisp
@@ -1,8 +1,36 @@
-;; || represents the cursor
+;;; -*- mode: erts; -*-
 
-(progn
-  ;; some comment
-  ||(+ 2 2))
+Point-Char: ¦
 
 ;; with paredit, you can't use paredit-splice-sexp-killing-backward
 ;; (M-up) without losing the comment
+Name: splice backward keeps comments
+Skip: t ; It's not implemented yet
+
+=-=
+(progn
+  ;; some comment
+  ¦(+ 2 2))
+=-=
+;; some comment
+¦(+ 2 2)
+=-=-=
+
+
+Name: contextual split sexp
+Skip: t ; It's not implemented yet
+
+=-=
+(:export
+ #:a
+ #:b¦
+ #:c
+ #:d)
+=-=
+(:export
+ #:a
+ #:b
+(:export
+ #:c
+ #:d)
+=-=-=
