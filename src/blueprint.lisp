25950ff97cf208cfef018f379e49be7a674540d5
Francis St-Amour <fr.stamour@gmail.com>
2025-10-08 20:44:12 -0400

    add empty file "blueprint.lisp"

---
+++ src/blueprint.lisp
@@ -0,0 +1,17 @@
+
+(defpackage #:breeze.blueprint
+  (:documentation "Snippets, but better™")
+  (:use #:cl))
+
+(in-package #:breeze.blueprint)
+
+;; TODO make "breeze-quickinsert" choose to insert a defun when there's a "defun" at point...
+defun
+
+
+defmethod insert-pattern
+
+patterns need docstring/documentation/prompt
+
+insert-symbol
+ensure-sym (convert symbol to sym)
