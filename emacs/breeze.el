c969544622dd71f96dffe21c85652ce32d13f746
Francis St-Amour <fr.stamour@gmail.com>
2025-10-14 06:23:46 -0400

    don't move the point around when replacing text before the point

--- emacs/breeze.el
+++ emacs/breeze.el
@@ -325,11 +325,9 @@ receiving the data it requested."
      (cl-destructuring-bind
          (point-from point-to replacement-string)
          (cdr request)
-       (let ((point (point)))
-         (delete-region (1+ point-from) (1+ point-to))
-         (goto-char (1+ point-from))
-         (insert replacement-string)
-         (goto-char point))))
+       (delete-region (1+ point-from) (1+ point-to))
+       (save-excursion (goto-char (1+ point-from))
+                       (insert replacement-string))))
     ("message"
      (breeze-message "%s" (cl-second request)))
     ("find-file"
