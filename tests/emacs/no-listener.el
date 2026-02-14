26b4bddda2f2652263b743711aa9ffc642553a83
Francis St-Amour <fr.stamour@gmail.com>
2026-02-14 15:31:15 -0500

    fix ~make test-emacs~

--- tests/emacs/no-listener.el
+++ tests/emacs/no-listener.el
@@ -1,8 +1,14 @@
 (require 'ert)
 (require 'ert-x)
 
+;; Remove any autoloaded listener functions (e.g. from guix site-level
+;; packages) so we can test the "no listener" scenario.
+(fmakunbound 'slime)
+(fmakunbound 'sly)
+(fmakunbound 'inferior-lisp)
+
 (ert-deftest breeze-init-no-listener-loaded ()
   "Make sure that `breeze-init' has sane behaviour if it can't find sly or slime."
   (should (equal
-           '(error "Please start either slime or sly.")
+           '(error "Please load either slime or sly.")
            (should-error (breeze-init)))))
